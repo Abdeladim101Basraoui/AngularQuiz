c8c8ae71b6024cb9c40e85bb6075f7a935b52eef
Basraoui101 <abdoobas101@gmail.com>
2021-12-14 15:40:20 +0100

    project architecrute

--- ngProject/src/app/app.module.ts
+++ ngProject/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// decorator ng module pour le cree il faut faire:
+// declaration == declare les web component a chaque fois il y a de nouveau component on les ajouter ici
+// import  == specifier les module qu'on peut les importer [BrowserModule] is always used
+//            httpClientModule  == interagir avec la partie backend
+//            formsModule == utiliser pour gerer les formulaire
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+
+  // pour les services de notre projet
+    providers: [],
+
+    // spicify the component root to start
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ngProject/src/main.ts
+++ ngProject/src/main.ts
@@ -8,5 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
+//apres l'exe du index.html ==>main.ts==>qui fait un bootstrap module de "appmodule" 
+// demarer un module == Appmodule
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
